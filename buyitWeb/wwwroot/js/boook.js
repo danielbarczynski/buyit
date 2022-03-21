d59c04e4c631de528bc1148fd8d246213461aa63
Daniel <daniel.barczynski@onet.pl>
2022-03-21 16:42:56 +0100

    book.js restored

---
+++ buyitWeb/wwwroot/js/boook.js
@@ -0,0 +1,56 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Book/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "author", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <button onClick=Delete('/Admin/Book/Delete/${data}')>Delete</buttton>
+                        `
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                    }
+                }
+            })
+        }
+    })
+}
+
+console.log("bye");
+
