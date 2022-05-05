0d1f344ee0fa01756bbdc526035427f771de38d4
Daniel PC <daniel.barczynski@onet.pl>
2022-05-05 18:26:32 +0200

    azure

---
+++ buyitWeb/wwwroot/js/book.js
@@ -0,0 +1,11 @@
+﻿"use strict";
+console.log("1");
+const ship = document.getElementById("ship");
+const trackingNumber = document.getElementById("trackingNumber");
+const carrier = document.getElementById("carrier");
+
+ship.addEventListener("onClick", function (event) {
+    console.log("2");
+    event.preventDefault();
+    alert("Fill out the tracking and carrier input!");
+});
